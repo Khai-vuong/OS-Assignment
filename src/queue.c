a2eade801b07bb154729e433d29c345d20471bfb
Vương Khải <khai.vuongquang@hcmut.edu.vn>
2024-05-13 16:44:28 +0700

    last update, GGWP

--- src/queue.c
+++ src/queue.c
@@ -1,32 +1,60 @@
+// timer_id_container_t * dev_list = NULL;
+// timer_id_container_t * dev_list = NULL;
 #include <stdio.h>
 #include <stdlib.h>
 #include "queue.h"
-
-int empty(struct queue_t * q) {
-        if (q == NULL) return 1;
-	return (q->size == 0);
+#define FALSE 0;
+int empty(struct queue_t *q)
+{
+        if (q == NULL)
+                return 1;
+        return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
         /* TODO: put a new process to queue [q] */
-        if (q->size < MAX_QUEUE_SIZE) {
-        q->proc[q->size] = proc ;
-        q->size++;
-}
+        if (q == NULL)
+        {
+                perror("Queue is NULL !\n");
+                exit(1);
+        }
+        int size = q->size;
+        if (size >= MAX_QUEUE_SIZE || size >= (MAX_PRIO - proc->prio))
+        {
+                perror("Queue is full !\n");
+                exit(1);
+        }
+        else
+        {
+                q->proc[size] = proc;
+                q->size++;
+        }
 }
 
-struct pcb_t * dequeue(struct queue_t * q) {
+struct pcb_t *dequeue(struct queue_t *q)
+{
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-        struct pcb_t * proc = NULL ;
-        if (q->size != 0) {
-                proc = q->proc[0];
-                q->size--;
-                for (int i = 0 ; i < q->size ; ++i ) {
-                        q -> proc[i] = q->proc [i +1];
-                }
+        if (q == NULL)
+        {
+                perror("Queue is NULL !\n");
+                exit(1);
         }
-        return proc ;
-}
-
+        int size = q->size;
+        if (size<=0)
+        {
+                return NULL;
+        }
+        else
+        {
+                struct pcb_t *result = q->proc[0];
+                for (int i = 0; i < size -1; i++)
+                {
+                        q->proc[i] = q->proc[i+1];
+                }
+                q->size--;
+                return result;
+        }
+}   
